e4c60e6d52ee21f70996cf4892171a448fa3beca
Jimmy <toluadejimi@gmail.com>
2024-06-17 17:16:16 +0100

    pushhh

--- app/Http/Controllers/EnkpayController.php
+++ app/Http/Controllers/EnkpayController.php
@@ -11,6 +11,43 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\PaymentRequest;
 use App\Traits\Processor;
 
+
+
+
+
+
+if (!function_exists('send_notification')) {
+
+    function send_notification($message)
+    {
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://api.telegram.org/bot6140179825:AAGfAmHK6JQTLegsdpnaklnhBZ4qA1m2c64/sendMessage?chat_id=1316552414',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => array(
+                'chat_id' => "1316552414",
+                'text' => $message,
+
+            ),
+            CURLOPT_HTTPHEADER => array(),
+        ));
+
+        $var = curl_exec($curl);
+        curl_close($curl);
+
+        $var = json_decode($var);
+    }
+}
+
+
 class EnkpayController extends Controller
 {
 
@@ -38,42 +75,28 @@ class EnkpayController extends Controller
     public function initialize(Request $request)
     {
 
-    
+
 
         $data = $this->payment::where(['id' => $request['payment_id']])->where(['is_paid' => 0])->first();
         if (!isset($data)) {
             return response()->json($this->response_formatter(GATEWAYS_DEFAULT_204), 200);
         }
 
-    
+        $payer = json_decode($data['payer_information']);
 
-      
+
+
         $key = env('WEBKEY');
-        $ref = BOOM();
-        $email = Auth::user()->email;
+        $ref = "BOOM".$data->attribute_id;
+        $email = $payer->email;
 
         $url = "https://web.enkpay.com/pay?amount=$data->payment_amount&key=$key&ref=$ref&email=$email";
 
 
-        $data = new Deposit();
-        $data->user_id = Auth::id();
-        $data->method_code = 102;
-        $data->method_currency = "NGN";
-        $data->amount = $request->amount;
-        $data->charge = 0;
-        $data->rate = 0;
-        $data->final_amo = $request->amount;
-        $data->btc_amo = 0;
-        $data->btc_wallet = "";
-        $data->trx = $ref;
-        $data->status = 2;
+        $message = $payer->email . "| wants to pay |  NGN " . number_format($data->payment_amount) . " | with ref | $ref |  on BOOMZY";
+        send_notification($message);
 
-        $data->save();
+        return redirect()->away($url);
 
-        $message = Auth::user()->email . "| wants to fund |  NGN " . number_format($request->amount) . " | with ref | $ref |  on ACEBOOSTSS";
-        send_notification_2($message);
-
-
-        return Redirect::to($url);
     }
 }
