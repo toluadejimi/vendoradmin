f7cd3dc343b43f84860951f7e0ede0c173542f47
Jimmy <toluadejimi@gmail.com>
2024-08-16 07:25:14 +0100

    vvbbb

--- app/Http/Controllers/Api/V1/OrderController.php
+++ app/Http/Controllers/Api/V1/OrderController.php
@@ -885,7 +885,7 @@ class OrderController extends Controller
                 if(!in_array($order->payment_method, ['digital_payment', 'partial_payment', 'offline_payment'])  || $payments){
                         Helpers::send_order_notification($order);
 
-                        if ($order->order_status == 'pending' && config('mail.status')) {
+                        if ($order->order_status == 'pending') {
                             Mail::to($admin_email)->send(new PlaceOrder($order->id));
                         }
 
